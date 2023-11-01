72cfd5aef7ad242ae7ab268d8c56127438aef723
laboratorio <laboratorio@UPOAULA10622>
2023-11-01 07:45:41 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoProgramación.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
