fd2012e7c67a47ebcd32c2a8f0a20163467c43b5
Juan Andres Alvarez Pullas <juan.alvarez.pullas@udla.edu.ec>
2023-11-06 08:44:59 -0500

    Cambio color de fondo

---
+++ Migrations/20231106131905_Clase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProyectoProgramación.Migrations
+{
+    public partial class Clase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
